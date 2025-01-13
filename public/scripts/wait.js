8e8530b533428ad517ba892fbf94eaa767121ad4
Shango <shangozonveni@gmail.com>
2025-01-13 23:47:05 +0200

    c3

---
+++ public/scripts/wait.js
@@ -0,0 +1,7 @@
+ export function name(linkContainer) {
+         if (document.querySelector('.js-btn')) {
+          document.querySelector('.js-btn').innerHTML = linkContainer.innerHTML
+         }
+           }
+  
+ 
